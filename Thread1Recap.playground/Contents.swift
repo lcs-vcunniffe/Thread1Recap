6405b4af2b03d5cfb9d631cf299c15201e2c49f3
Veda Cunniffe <vcunniffe@lcs.on.ca>
2024-01-09 12:00:29 -0500

    Made the dog structure.

--- Thread1Recap.playground/Contents.swift
+++ Thread1Recap.playground/Contents.swift
@@ -1,3 +1,16 @@
 import Cocoa
 
 var greeting = "Hello, playground"
+
+// MARK: Stored properties
+struct Dog {
+    let breed: String
+    let color: String
+    var heightInMetres: Double
+    
+    // MARK: Computed properties
+    var heightInCentimetres: Double {
+        return heightInMetres * 100
+    }
+}
+
